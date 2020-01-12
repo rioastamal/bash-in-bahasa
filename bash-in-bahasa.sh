ec783f482adb3909fcf127f11fea0ad07c9a6a53
Rio Astamal <me@rioastamal.net>
2020-01-12 23:45:14 +0700

    Initial commit Bash for Bahasa Indonesia

---
+++ bash-in-bahasa.sh
@@ -0,0 +1,91 @@
+#!/bin/bash
+#
+# List of alias of Bash keywords in Bahasa Indonesia
+#
+# @author Rio Astamal <rio@rioastamal.net>
+# @license MIT
+
+# Generated using `compgen -k`
+alias jika=if
+alias jk=if
+alias maka=then
+alias selain_itu=else
+alias seltu=else
+alias selain_itu_jika=elif
+alias seltu_jika=elif
+alias kaji=fi
+alias akhir_jika=fi
+alias kj=fi
+alias kasus=case
+alias hal=case
+alias susak=esac
+alias lah=esac
+alias untuk=for
+alias pilih=select
+alias selama=while
+alias sampai=until
+alias lakukan=do
+alias selesai=done
+alias dalam=in
+alias fungsi=function
+alias waktu=time
+
+# Generated using `compgen -b`
+alias namalain=alias
+alias proses_latar=bg
+alias mengikat=bind
+alias putus=break
+alias berhenti=break
+#builtin
+alias pemanggil=caller
+alias masuk=cd
+alias perintah=command
+#compgen
+alias lengkap=complete
+alias teruskan=continue
+alias deklarasikan=declare
+#dirs
+alias pungkiri=disown
+alias cetak=echo
+alias tulis=echo
+alias izinkan=enable
+alias evaluasi=eval
+alias eksekusi=exec
+alias keluar=exit
+#exporexport
+alias salah=false
+#fc
+alias proses_depan=fg
+alias ambilopsi=getopts
+# hash
+alias bantuan=help
+alias riwayat=history
+alias kerjaan=jobs
+alias bunuh=kill
+alias biarkan=let
+alias lokal=local
+# logout
+# popd
+alias cetakf=printf
+# pushd
+alias dir_sekarang=pwd
+alias baca=read
+alias baca_input=read
+alias bacasaja=readonly
+alias kembalian=return
+alias tetapkan=set
+alias geser_arg=shift
+# shopt
+alias sumber=source
+alias tangguhkan=suspend
+alias coba=test
+alias akumulasi_waktu=times
+alias jebakan=trap
+alias benar=true
+alias tipe=type
+#typeset
+#ulimit
+#umask
+alias hilangkan_namalain=unalias
+alias jangan_tetapkan=unset
+alias tunggu=wait
